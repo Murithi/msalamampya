d78602b0e693f5da61c3537c0de6b647bfe22c24
Eric <murithier.2006@gmail.com>
2016-06-15 16:06:10 +0300

    remove django-forms

--- msalamampya/settings.py
+++ msalamampya/settings.py
@@ -47,7 +47,7 @@ INSTALLED_APPS = (
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'bootstrap3',
-    'django_forms_bootstrap',
+    # 'django_forms_bootstrap',
     'django_extensions',
     'django_messages',
     'users',
